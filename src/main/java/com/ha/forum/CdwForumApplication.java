d688e22ca5b0d11793af2610c91066615a6499e6
hazoe-dev <hatran.dev@gmail.com>
2022-05-22 16:20:09 +0700

    Setup project

---
+++ src/main/java/com/ha/forum/CdwForumApplication.java
@@ -0,0 +1,13 @@
+package com.ha.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdwForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdwForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ha/forum/CdwForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ha.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdwForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
